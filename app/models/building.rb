7ff5f3d34c636b5b39dc424b6317e14054240383
szyar <soezayarszyar@gmail.com>
2020-09-01 11:17:05 +0630

    add stations controller and test view file

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,5 @@
 class Building < ApplicationRecord
   has_many :stations, dependent: :destroy
-  accepts_nested_attributes_for :stations, allow_destroy: true
+  accepts_nested_attributes_for :stations, allow_destroy: true,
+    reject_if: proc { |attributes| attributes['station_name'].blank? }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :buildings
   root 'buildings#index'
+  resources :stations
 end
