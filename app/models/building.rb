57ba3a9ec652bc5fcdab0ff5e80d6f112d7df0eb
szyar <soezayarszyar@gmail.com>
2020-09-01 11:33:54 +0630

    add validations to buildings

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,8 @@
 class Building < ApplicationRecord
+  validates :name, presence: true
+  validates :rental_fees, presence: true
+  validates :address, presence: true
+  validates :build_year, presence: true
   has_many :stations, dependent: :destroy
   accepts_nested_attributes_for :stations, allow_destroy: true,
     reject_if: proc { |attributes| attributes['station_name'].blank? }
